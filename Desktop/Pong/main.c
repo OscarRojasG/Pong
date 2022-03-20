f482a95c76995524bfdac9d494c5c8f2370f46ee
Your Name <you@example.com>
2022-03-20 14:09:15 -0300

    code refactoring

--- Desktop/Pong/main.c
+++ Desktop/Pong/main.c
@@ -11,19 +11,28 @@
 
 struct {
     SDL_Rect *shape;
+    double velocityY;
+    double motionY;
+} typedef PingPaddle;
+
+struct {
+    SDL_Rect *shape;
+    double velocity;
     double velocityX;
     double velocityY;
     double motionX;
     double motionY;
-} typedef PingRect;
+    double angle;
+} typedef PingBall;
 
-PingRect *createRect(int, int, int, int, double, double);
-void movePaddleLeft(PingRect *, PingRect *);
-void movePaddleRight(PingRect *, int);
-void moveBall(PingRect *, double);
-bool touchingBorderX(PingRect *);
-bool touchingBorderY(PingRect *);
-bool checkColission(PingRect *, PingRect *);
+PingPaddle *createPaddle(int, int, int, int, double);
+PingBall *createBall(int, int, int, int, double, double, double, double);
+void moveLeftPaddle(PingPaddle *, PingBall *);
+void moveRightPaddle(PingPaddle *, int, double);
+void moveBall(PingBall *, double);
+bool touchingBorderX(SDL_Rect *);
+bool touchingBorderY(SDL_Rect *);
+bool checkColission(PingPaddle *, PingBall *);
 
 int main(int argc, char** argv){
     if(SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
@@ -41,9 +50,9 @@ int main(int argc, char** argv){
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer) return 1;
 
-    PingRect *leftPaddle = createRect(50, (SCREEN_HEIGHT - 80) / 2, 15, 80, 0, 200);
-    PingRect *rightPaddle = createRect(SCREEN_WIDTH - 65, (SCREEN_HEIGHT - 80) / 2, 15, 80, 0, -200);
-    PingRect *ball = createRect((SCREEN_WIDTH - 15) / 2, (SCREEN_HEIGHT - 15) / 2, 15, 15, 200, 200);
+    PingPaddle *leftPaddle = createPaddle(50, (SCREEN_HEIGHT - 80) / 2, 15, 80, 200);
+    PingPaddle *rightPaddle = createPaddle(SCREEN_WIDTH - 65, (SCREEN_HEIGHT - 80) / 2, 15, 80, 300);
+    PingBall *ball = createBall((SCREEN_WIDTH - 15) / 2, (SCREEN_HEIGHT - 15) / 2, 15, 15, 300, 300, 300, 45);
 
     Uint32 lastUpdate = SDL_GetTicks();
     SDL_Event event;
@@ -89,11 +98,11 @@ int main(int argc, char** argv){
         Uint32 current = SDL_GetTicks();
         double dT = (current - lastUpdate) / 1000.0f; 
 
-        movePaddleLeft(leftPaddle, ball);
-        movePaddleRight(rightPaddle, direction);
+        moveLeftPaddle(leftPaddle, ball);
+        moveRightPaddle(rightPaddle, direction, dT);
         moveBall(ball, dT);
-        checkColission(ball, leftPaddle);
-        checkColission(ball, rightPaddle);
+        checkColission(leftPaddle, ball);
+        checkColission(rightPaddle, ball);
 
         lastUpdate = current;
 
@@ -107,85 +116,110 @@ int main(int argc, char** argv){
     return 0;
 }
 
-PingRect *createRect(int x, int y, int w, int h, double vx, double vy) {
-    PingRect *rect = (PingRect *) malloc(sizeof(PingRect));
-    rect->shape = (SDL_Rect *) malloc(sizeof(SDL_Rect)); 
+PingPaddle *createPaddle(int x, int y, int w, int h, double vy) {
+    PingPaddle *paddle = (PingPaddle *) malloc(sizeof(PingPaddle));
+    paddle->shape = (SDL_Rect *) malloc(sizeof(SDL_Rect)); 
 
-    rect->shape->x = x;
-    rect->shape->y = y;
-    rect->shape->w = w;
-    rect->shape->h = h;
-    rect->velocityX = vx;
-    rect->velocityY = vy;
-    rect->motionX = 0;
-    rect->motionY = 0;
+    paddle->shape->x = x;
+    paddle->shape->y = y;
+    paddle->shape->w = w;
+    paddle->shape->h = h;
+    paddle->velocityY = vy;
+    paddle->motionY = 0;
 
-    return rect;
+    return paddle;
 }
 
-void movePaddleLeft(PingRect *rect, PingRect *ball) {	
+PingBall *createBall(int x, int y, int w, int h, double v, double vx, double vy, double a) {
+    PingBall *ball = (PingBall *) malloc(sizeof(PingBall));
+    ball->shape = (SDL_Rect *) malloc(sizeof(SDL_Rect));
+
+    ball->shape->x = x;
+    ball->shape->y = y;
+    ball->shape->w = w;
+    ball->shape->h = h;
+    ball->velocity = v;
+    ball->velocityX = vx;
+    ball->velocityY = vy;
+    ball->angle = a;
+    ball->motionX = 0;
+    ball->motionY = 0;
+
+    return ball;
+}
+
+void moveLeftPaddle(PingPaddle *paddle, PingBall *ball) {	
 	int centerY = ball->shape->y + (ball->shape->h / 2);
-    int posY = centerY - (rect->shape->h / 2);
+    int posY = centerY - (paddle->shape->h / 2);
 
-    if(touchingBorderY(rect)) {
+    if(touchingBorderY(paddle->shape)) {
         if(posY < 0) posY = 0; 
-        if(posY + rect->shape->h > SCREEN_HEIGHT) posY = SCREEN_HEIGHT - rect->shape->h;
+        if(posY + paddle->shape->h > SCREEN_HEIGHT) posY = SCREEN_HEIGHT - paddle->shape->h;
     }
-    rect->shape->y = posY;
+    paddle->shape->y = posY;
 }
 
-void movePaddleRight(PingRect *rect, int direction) {	
-    if(direction == MOVE_STATIC) return;
-	if(direction == MOVE_UP) rect->shape->y += -1;
-    if(direction == MOVE_DOWN) rect->shape->y += 1;
-
-    if(touchingBorderY(rect)) {
-        if(rect->shape->y < 0) rect->shape->y = 0; 
-        if(rect->shape->y + rect->shape->h > SCREEN_HEIGHT) rect->shape->y = SCREEN_HEIGHT - rect->shape->h;
+void moveRightPaddle(PingPaddle *paddle, int direction, double time) {	
+    if(direction == MOVE_STATIC) {
+        paddle->motionY = 0;
+        return;
     }
-}
 
-void moveBall(PingRect *rect, double time) {
-    rect->motionX += rect->velocityX * time;
-    rect->motionY += rect->velocityY * time;
+	if(direction == MOVE_UP) paddle->motionY += paddle->velocityY * time * -1;
+    if(direction == MOVE_DOWN) paddle->motionY += paddle->velocityY * time;
 
-    if(abs(rect->motionX) >= 1) {
-        rect->shape->x += (long)rect->motionX;
-        rect->motionX = rect->motionX - (long)rect->motionX;
+    if(abs(paddle->motionY) >= 1) {
+        paddle->shape->y += (long)paddle->motionY;
+        paddle->motionY = paddle->motionY - (long)paddle->motionY;
 
-        if(touchingBorderX(rect)) {
-            rect->velocityX *= -1; 
-            rect->motionX *= -1;
-        }
-    }
-    if(abs(rect->motionY) >= 1) {
-        rect->shape->y += (long)rect->motionY;
-        rect->motionY = rect->motionY - (long)rect->motionY;
-    
-        if(touchingBorderY(rect)) {
-            rect->velocityY *= -1; 
-            rect->motionY *= -1;
+        if(touchingBorderY(paddle->shape)) {
+            if(paddle->shape->y < 0) paddle->shape->y = 0; 
+            if(paddle->shape->y + paddle->shape->h > SCREEN_HEIGHT) paddle->shape->y = SCREEN_HEIGHT - paddle->shape->h;
         }
     }
 }
 
-bool touchingBorderX(PingRect *rect) {
-    if(rect->shape->x == 0) return true;
-    if(rect->shape->x + rect->shape->w == SCREEN_WIDTH) return true;
+void moveBall(PingBall *ball, double time) {
+    ball->motionX += ball->velocityX * time;
+    ball->motionY += ball->velocityY * time;
+
+    if(abs(ball->motionX) >= 1) {
+        ball->shape->x += (long)ball->motionX;
+        ball->motionX = ball->motionX - (long)ball->motionX;
+
+        if(touchingBorderX(ball->shape)) {
+            ball->velocityX *= -1; 
+            ball->motionX *= -1;
+        }
+    }
+
+    if(abs(ball->motionY) >= 1) {
+        ball->shape->y += (long)ball->motionY;
+        ball->motionY = ball->motionY - (long)ball->motionY;
+
+        if(touchingBorderY(ball->shape)) {
+            ball->velocityY *= -1; 
+            ball->motionY *= -1;
+        }
+    }
+}
+
+bool touchingBorderX(SDL_Rect *rect) {
+    if(rect->x == 0) return true;
+    if(rect->x + rect->w == SCREEN_WIDTH) return true;
     return false;
 }
 
-bool touchingBorderY(PingRect *rect) {
-    if(rect->shape->y <= 0) return true;
-    if(rect->shape->y + rect->shape->h >= SCREEN_HEIGHT) return true;
+bool touchingBorderY(SDL_Rect *rect) {
+    if(rect->y <= 0) return true;
+    if(rect->y + rect->h >= SCREEN_HEIGHT) return true;
     return false;
 }
 
-bool checkColission(PingRect *ball, PingRect *paddle) {
+bool checkColission(PingPaddle *paddle, PingBall *ball) {
     SDL_Rect intersection;
     if(SDL_IntersectRect(ball->shape, paddle->shape, &intersection)) {
-        if(ball->shape->y + ball->shape->h >= paddle->shape->y &&
-        ball->shape->y <= paddle->shape->y + paddle->shape->h) {
+        if(intersection.h >= ball->shape->h / 2) {
             ball->velocityX *= -1;
             if(ball->motionX < 0) ball->motionX = intersection.w;
             else ball->motionX = intersection.w * -1;
